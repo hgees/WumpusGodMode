f05344c513814dba321c25ab76d940f986d68e14
Guido De Lyra Ferrario <guido_ferrario@hotmail.com>
2015-12-20 17:37:36 -0300

    corrigindo erro

--- agente002.pl
+++ agente002.pl
@@ -525,14 +525,71 @@ casasvisitadas :- %funcao que salva casas visitadas
 
 %funcao para calcular casas seguras
 versegura:-
+    casasegura0,       
+    casasegura90,     
+    casasegura180,       
+    casasegura270,      
+    tiraperigosas,     
+    reduz.
+
+casasegura0:-
     seguras(S),
     posicao([X,Y]),
-    adjacentes([X,Y], L),
+    X<4,
+    Z is X+1,
+    not(member([Z,Y],S)),
+    append(S,[[Z,Y]],S1),
     retractall(seguras(_)),
-    append(L,S, S1),
-    append([X,Y], S1, S2),
-    list_to_set(S2, S3),
-    assert(seguras(S3)).
+    assert(seguras(S1)).
+casasegura0.
+
+casasegura90:-
+    seguras(S),
+    posicao([X,Y]),
+    Y<4,
+    Z is Y+1,
+    not(member([X,Z],S)),
+    append(S,[[X,Z]],S1),
+    retractall(seguras(_)),
+    assert(seguras(S1)).
+casasegura90.
+
+casasegura180:-
+    seguras(S),
+    posicao([X,Y]),
+    X>1,
+    Z is X-1,
+    not(member([Z,Y],S)),
+    append(S,[[Z,Y]],S1),
+    retractall(seguras(_)),
+    assert(seguras(S1)).
+casasegura180.
+
+casasegura270:-
+    seguras(S),
+    posicao([X,Y]),
+    Y>1,
+    Z is Y-1,
+    not(member([X,Z],S)),
+    append(S,[[X,Z]],S1),
+    retractall(seguras(_)),
+    assert(seguras(S1)).
+casasegura270.
+
+tiraperigosas:-
+    seguras(S),
+    perigosas(P),
+    subtract(S,P, L),
+    retractall(seguras(_)),
+    assert(seguras(L)).
+tiraperigosas.
+
+reduz:-
+    seguras(S),
+    list_to_set(S,S1),
+    retractall(seguras(_)),
+    assert(seguras(S1)).
+reduz.
 
 
 %funcao para calcular casas que oferecem risco ao agente
